29972ccf4e4dec5e15ef03f155a223b520c760ef
optimalcycling <optimalcycling@575be3a7-a259-01e7-bbfe-c75e66796aff>
2010-07-27 02:10:21 +0000

    First public version

---
+++ background.js
@@ -0,0 +1,24 @@
+
+chrome.extension.onRequest.addListener(function(msg, src, send) {
+
+		var urlInWhiteList = isWhitelisted(msg.url);
+		var urlsGloballyAllowed = config.get('globalAllowAll');    
+		
+		send({enabled: (urlInWhiteList || urlsGloballyAllowed), 
+			blockWindowOpen: config.get('blockWindowOpen'),  
+			closeAllPopUpWindows: config.get('closeAllPopUpWindows'), 
+			
+			blockWindowPrompts: config.get('blockWindowPrompts'), 
+			blockJSContextMenuAndClickIntercept: config.get('blockJSContextMenuAndClickIntercept'),
+			blockWindowMovingAndResize: config.get('blockWindowMovingAndResize'), 
+			blockUnescapeEval: config.get('blockUnescapeEval'), 
+			blockJSSelection: config.get('blockJSSelection'), 
+			blockJSTimers: config.get('blockJSTimers'), 
+			blockJSPrint: config.get('blockJSPrint'), 
+			blockOnUnload: config.get('blockOnUnload'),
+			blockWindowTargets: config.get('blockWindowTargets'),
+			
+			extendedTooltips: config.get('extendedTooltips'),
+			
+			blockCreateEvents: config.get('blockCreateEvents')}); 
+});
